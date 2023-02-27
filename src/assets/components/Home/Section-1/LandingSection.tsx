354d6718469e67b070000c24fe55d25a35894ad7
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-02-27 19:54:04 -0400

    updated responsiveness and grid system

--- src/assets/components/Home/Section-1/LandingSection.tsx
+++ src/assets/components/Home/Section-1/LandingSection.tsx
@@ -1,50 +1,41 @@
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import { ImageAndText } from "../../../interfaces/LandingSectionProps";
+import myPhoto from "../../../../images/projects-images/img.jpg";
+import "../../../style-utils/utils.css";
 
-const LandingSection = ({ image }: ImageAndText) => {
+const LandingSection = () => {
   return (
     <div className="bg-main-extra-dark-blue min-h-screen flex text-center justify-center items-center">
-      {" "}
-      {/* the actual box 1 */}
-      <div className="grid-flow-row grid grid-cols-2 items-center text-center max-1023:grid-cols-none grid-rows-none flex-none">
-        {" "}
-        {/*box 1 grid system for positioning both the image and the content*/}
-        <div className="box-1 flex py-32 max-1023:items-center max-1023:justify-center max-639:px-8">
-          {" "}
-          {/*box 1 content*/}
-          <div className="flex-1 w-full max-w-max items-start max-1023:m-0">
-            {" "}
-            {/* text and links */}
-            <span className="text-white font-bold text-3xl hover:text-main-cyan duration-300">
-              Hi, I am Francisco Luna
-            </span>
-            <span className="font-bold text-main-cyan text-5xl hover:text-white duration-300 block pt-2">
-              Front End Developer
-            </span>
-            <div className="flex justify-center">
-              <p className="text-silver max-w-lg leading-normal pt-2 max-639:max-w-sm max-639:leading-loose text-lg text-justify items-start justify-start flex">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
-                tincidunt egestas odio, a suscipit nunc varius quis. Lorem ipsum
-                dolor sit amet, consectetur adipiscing elit.
-              </p>
-            </div>
-            {/* button and icons positioning */}
-            <div className="flex justify-items-start gap-4 items-center pt-4 max-1023:justify-center">
-              <button className="bg-transparent hover:bg-main-cyan duration-300 ease-in h-14 text-silver font-regular hover:text-main-extra-dark-blue py-2 px-4 border border-main-cyan hover:border-transparent rounded max-1023:h-16">
-                Contact me
-              </button>
-              <GitHubIcon className="text-white cursor-pointer" />
-              <LinkedInIcon className="text-white cursor-pointer" />
-            </div>
-          </div>
-        </div>
-        <div className="flex justify-start items-center">
+      <div className="grid grid-cols-2 gap-10 max-1279:flex max-1279:grid-cols-none">
+        <div className="order-last flex justify-center items-center max-1279:hidden">
           <img
-            className="rounded-full h-72 ml-36 border-main-cyan border-2 shadow-xl hover:scale-110 duration-500 
-    ease-out max-1023:hidden"
-            src={image}
-          />
+            className="h-72 w-72 rounded-full glow-main border-white border-2 border-double main-image p-1 floating"
+            src={myPhoto}
+          ></img>
+        </div>
+        <div className="max-639:px-5">
+          <h1 className="text-white font-bold text-5xl flex py-2 max-639:justify-center max-639:text-4xl">
+            Hi, I am Francisco Luna
+          </h1>
+          <h2 className="text-main-cyan font-bold text-4xl flex py-2 glow-text max-639:justify-center max-639:text-3xl">
+            Front End Developer
+          </h2>
+          <div>
+            <p className="text-silver leading-loose py-2 text-left w-96 max-639:w-72 max-639:m-auto max-639:text-center text-xl">
+              18 years old Front End Developer from Venezuela. Currently
+              studying a Computer Science Major and working on personal
+              projects.
+            </p>
+          </div>
+
+          <div className="flex items-center gap-4 m-0 py-4 max-639:justify-center">
+            <button className="bg-transparent hover:bg-main-cyan flex justify-items-start justify-center items-center duration-300 ease-in h-14 text-silver font-regular hover:text-main-extra-dark-blue w-1/2 border border-main-cyan hover:border-transparent rounded glow-secondary">
+              Contact Me
+            </button>
+            {/*       <GitHubIcon className="text-white scale-110 max-639:hidden max-639:opacity-0"/>
+      <LinkedInIcon className="text-white scale-110 max-639:hidden max-639:opacity-0"/> */}
+          </div>
         </div>
       </div>
     </div>
