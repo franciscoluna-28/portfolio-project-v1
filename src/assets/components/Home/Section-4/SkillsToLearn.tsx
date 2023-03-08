217e2b4577dd06ccada6d04ace84aa7ba4d07de3
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-03-08 12:17:45 -0400

    shorten logic

--- src/assets/components/Home/Section-4/SkillsToLearn.tsx
+++ src/assets/components/Home/Section-4/SkillsToLearn.tsx
@@ -1,21 +1,40 @@
-import NodeJSIcon from "../../../../images/skills-icons/nodejs.png";
 import MySQLIcon from "../../../../images/skills-icons/mysql.png";
 import BoostrapIcon from "../../../../images/skills-icons/boostrap.png";
 import NextJSIcon from "../../../../images/skills-icons/nextjs.png";
-import SkillComponent from "./SkillComponent";
+import Skill from "./Skill";
+
+const toLearnSkillsIcons = [{
+  id: 1,
+  icon: BoostrapIcon,
+  name: "Boostrap"
+},
+{
+  id: 2,
+  icon: NextJSIcon,
+  name: "Nextjs"
+},
+{
+  id: 3,
+  icon: MySQLIcon,
+  name: "MySQL"
+}]
 
 const SkillsToLearn = () => {
   return (
-    <div className="w-1/2 h-36 bg-gradient-to-r from-main-extra-dark-blue to-main-++dark-blue m-auto my-8 rounded overflow-hidden flex hover:scale-105 duration-500 ease-in-out">
-      <div className="flex justify-center m-auto gap-4 w-auto">
-        <SkillComponent iconImage={NodeJSIcon} />
-        <SkillComponent iconImage={MySQLIcon} />
-        <SkillComponent iconImage={BoostrapIcon} />
-        <div className="h-full w-32 bg-white px-2 rounded justify-center flex"></div>
-        <SkillComponent iconImage={NextJSIcon} />
-      </div>
-    </div>
+    <>
+      {toLearnSkillsIcons.map(skill => (
+        <Skill id={skill.id} icon={skill.icon} name={skill.name} />
+      ))}
+    </>
   );
-};
+}
+
+
+
+
+
+
+
+
 
 export default SkillsToLearn;
