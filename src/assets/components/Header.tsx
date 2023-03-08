31e2a5b9658d15396c61f08e5ca0882b146c132b
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-03-08 12:15:00 -0400

    updated header responsive design

--- src/assets/components/Header.tsx
+++ src/assets/components/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
       <div className="py-6 flex justify-center
       w-full m-0 sticky top-0 overflow-auto self-start z-20 bg-main-extra-dark-blue shadow-md">
 
-          <ul className="ulNav">
+          <ul className="ulNav max-1023:hidden">
             <li className="liNav">
               <a href="#">
                 <span className="link link-underline link-underline-black text-gray">
