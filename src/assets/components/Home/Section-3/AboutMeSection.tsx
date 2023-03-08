a4987ec7de447a4c6d90d0c357647ebf62711346
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-03-08 12:16:21 -0400

    added a new card

--- src/assets/components/Home/Section-3/AboutMeSection.tsx
+++ src/assets/components/Home/Section-3/AboutMeSection.tsx
@@ -1,7 +1,7 @@
 import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
-import codingBg from "../../../../images/projects-images/coding-bg.jpg"
-import mernStack from "../../../../images/projects-images/mern.png"
-import frontEnd from "../../../../images/projects-images/dev.jpg"
+import codingBg from "../../../../images/projects-images/coding-bg.jpg";
+import mernStack from "../../../../images/projects-images/mern.png";
+import frontEnd from "../../../../images/projects-images/dev.jpg";
 
 const AboutMeSection = () => {
   return (
@@ -9,40 +9,44 @@ const AboutMeSection = () => {
       <h2 className="pt-8 font-extrabold text-white uppercase text-4xl tracking-wider main-title main-title-glow">
         About Me
       </h2>
-      <div className="py-12 px-64">
-        <ol className="relative border-l border-gray-200 dark:border-gray-700">
-          <div className="flex">
-            <li className="mb-10 ml-4">
-              <time className="text-md mb-1 font-normal leading-none text-gray-400 dark:text-gray-500 flex">
+      <div className="py-12 px-32 max-639:px-16">
+        <ol className="relative border-l border-gray-200 dark:border-gray-700 max-639:justify-center">
+          <div className="flex justify-center">
+            <li className="mb-10">
+              <time className="text-md mb-1 font-normal leading-none text-gray-400 dark:text-gray-500 flex text-left">
                 Introduction
               </time>
               <div className="flex">
-                <h3 className="font-bold flex text-white pt-2 text-2xl">
+                <h3 className="font-bold text-left flex text-white pt-2 text-2xl">
                   Who Am I?
                 </h3>
-                <span className="font-bold text-white border-solid border-2 border-main-cyan rounded-full w-6 h-6 text-center flex items-center justify-center glow-secondary mt-2.5 ml-2">
+                <span className="font-bold text-white border-solid border-2 border-main-cyan rounded-full w-6 h-6 text-center flex items-center justify-center glow-secondary mt-2.5 ml-2 max-639:hidden">
                   1
                 </span>
               </div>
               <p className="mb-4 text-base leading-loose font-normal text-gray-400 w-full pt-2 text-left">
-                Greetings, my name is Francisco Luna, an 18-year-old Front End
-                Developer hailing from Venezuela. In this portfolio, I will
-                share my journey and experiences with web development.
+                Greetings, my name is Francisco Luna, and I am an 18-year-old
+                computer science student and self taught front-end developer
+                from Venezuela. In this portfolio, I will share my journey and
+                experiences with web development.
               </p>
-             <img className="pt-6 overflow-hidde shadow-md hover:scale-105 duration-500 h-96 m-auto" src={frontEnd}/>
+{/*               <img
+                className="pt-6 overflow-hidde shadow-md hover:scale-105 duration-500 h-96 m-auto"
+                src={frontEnd}
+              /> */}
             </li>
           </div>
 
           <div className="flex justify-center">
-            <li className="mb-10 ml-4 pt-16">
-              <time className="mb-1 flex text-sm font-normal leading-none text-gray-500 dark:text-gray-500">
+            <li className="mb-10 pt-16">
+              <time className="mb-1 flex text-sm font-normal leading-none text-gray-500 dark:text-gray-500 text-left">
                 July 2021 - December 2022
               </time>
               <div className="flex">
-                <h3 className="font-bold flex text-white pt-2 text-2xl">
+                <h3 className="font-bold flex text-white pt-2 text-2xl text-left">
                   My Story with Coding
                 </h3>
-                <span className="font-bold text-white border-solid border-2 border-main-cyan rounded-full w-6 h-6 text-center flex items-center justify-center ambience mt-2.5 ambience ml-2">
+                <span className="font-bold text-white border-solid border-2 border-main-cyan rounded-full w-6 h-6 text-center flex items-center justify-center ambience mt-2.5 ambience ml-2 max-639:hidden">
                   2
                 </span>
               </div>
@@ -60,20 +64,23 @@ const AboutMeSection = () => {
                 me to further enhance my skills and broaden my understanding of
                 web development.
               </p>
-              <img className="pt-12 overflow-hidden shadow-md hover:scale-105 duration-500 h-96 w-fit m-auto" src={codingBg}/>
+{/*               <img
+                className="pt-12 overflow-hidden shadow-md hover:scale-105 duration-500 h-96 w-fit m-auto"
+                src={codingBg}
+              /> */}
             </li>
           </div>
 
           <div className="flex justify-center">
-            <li className="ml-4 pt-16">
-              <time className="mb-1 flex text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+            <li className="pt-16">
+              <time className="mb-1 flex text-sm text-left font-normal leading-none text-gray-400 dark:text-gray-500">
                 December 2022 - Currently
               </time>
-              <div className="flex">
-                <h3 className="font-bold flex text-white pt-2 text-2xl">
+              <div className="flex justify-start">
+                <h3 className="font-bold flex text-white pt-2 text-2xl text-left">
                   Current Goals and Aspirations
                 </h3>
-                <span className="font-bold text-white border-solid border-2 border-main-cyan rounded-full w-6 h-6 text-center flex items-center justify-center ambience mt-2.5 ambience ml-2">
+                <span className="font-bold text-white border-solid border-2 border-main-cyan rounded-full w-6 h-6 text-center flex items-center justify-center ambience mt-2.5 ambience ml-2 max-639:hidden">
                   3
                 </span>
               </div>
@@ -90,7 +97,10 @@ const AboutMeSection = () => {
                 from experienced developers, and I believe that working
                 collaboratively is the best way to achieve these goals.
               </p>
-              <img className="pt-12 overflow-hidde shadow-md hover:scale-105 duration-500 h-96 m-auto" src={mernStack}/>
+{/*               <img
+                className="pt-12 overflow-hidde shadow-md hover:scale-105 duration-500 h-96 m-auto"
+                src={mernStack}
+              /> */}
             </li>
           </div>
         </ol>
