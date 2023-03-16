c6fd10c2cff50182d3ef528bd78ea5659ec4088b
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-03-16 15:13:49 -0400

    minor updates

--- src/assets/components/Home/Section-4/SkillsSection.tsx
+++ src/assets/components/Home/Section-4/SkillsSection.tsx
@@ -2,13 +2,17 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDownload } from "@fortawesome/free-solid-svg-icons";
 import SkillsToLearn from "./SkillsToLearn";
 import LearnedSkills from "./SkillsLearned";
+import ArchitectureIcon from "@mui/icons-material/Architecture";
 
 const SkillsSection = () => {
   return (
-    <div className="w-full h-full bg-main-dark-extra-blue m-auto min py-16 px-32 max-639:px-16">
+    <div
+      className="w-full h-full bg-main-dark-extra-blue m-auto min py-8 px-32 max-639:px-16"
+      id="skills"
+    >
       <div className="glow-text">
         <h2 className="pt-8 font-extrabold text-white uppercase text-4xl tracking-wider main-title main-title-glow">
-          tech stack
+          tech stack and resume
         </h2>
       </div>
       <br />
@@ -21,19 +25,56 @@ const SkillsSection = () => {
           such as Figma, and for productivity, such as Taskade and Vite.
         </p>
 
-        <section className="flex flex-wrap gap-4 pt-12">
+        <div className="flex py-6 items-center">
+          <p className="inline-block text-white font-bold ml-2 text-left">
+            My current tech stack:
+          </p>
+        </div>
+
+        <section className="flex mt-4 duration-500 flex-wrap gap-4 py-10 px-4 project-skill-card shadow-lg rounded items-center">
           <LearnedSkills></LearnedSkills>
         </section>
 
         <p className="pt-12 w-full m-auto text-md text-gray-400 leading-loose text-left">
-          Also, those are the technologies that I'm either learning or I'm keen
-          on learning these months, according to my goal of being a MERN stack
-          developer and create more CRUD applications with complex features.
+          As a MERN stack developer, my primary goal is to build dynamic web
+          applications with complex features. To achieve this goal, I am
+          constantly learning and keeping up-to-date with the latest
+          technologies and frameworks. Currently, I am focusing on learning or
+          enhancing my skills in several technologies that are essential for
+          MERN stack development. These include MongoDB, Express.js, React, and
+          Node.js. As a MERN stack developer, I understand the importance of
+          being proficient in both front-end and back-end development. To
+          further my skills in back-end development, I am keen on learning
+          several new technologies.
         </p>
 
-        <section className="flex flex-wrap gap-4 pt-6">
+        <div className="flex py-6 items-center">
+          <p className="inline-block text-white font-bold ml-2 leading-loose text-left">
+            Technologies that I'll be learning:
+          </p>
+        </div>
+
+        <section className="flex mt-4 flex-wrap gap-4 w-auto py-10 px-4 project-skill-card shadow-lg rounded items-center">
           <SkillsToLearn></SkillsToLearn>
         </section>
+
+        <div className="flex pt-8 items-center">
+          <p className="inline-block text-white font-bold ml-2 leading-loose text-left">
+            My resume and professional profile:
+          </p>
+        </div>
+
+        <p className="w-full pt-4 m-auto text-md text-gray-400 leading-loose text-left">
+          As a bilingual MERN stack developer, I understand the importance of
+          effective communication in the industry. That's why I am proud to
+          offer my resume in both Spanish and English for your convenience.
+        </p>
+        <button className="bg-transparent hover:bg-main-cyan flex justify-items-start justify-center items-center duration-300 ease-in h-14 text-silver font-regular hover:text-main-extra-dark-blue w-full mt-6 border border-main-cyan hover:border-transparent rounded glow-secondary">
+          Resume (Spanish)
+        </button>
+        <button className="bg-transparent hover:bg-main-cyan flex justify-items-start justify-center items-center duration-300 ease-in h-14 text-silver font-regular hover:text-main-extra-dark-blue w-full mt-6 border border-main-cyan hover:border-transparent rounded glow-secondary">
+          Resume (English)
+        </button>
       </div>
     </div>
   );
--- src/assets/components/Home/Section-4/SkillsToLearn.tsx
+++ src/assets/components/Home/Section-4/SkillsToLearn.tsx
@@ -1,40 +1,52 @@
 import MySQLIcon from "../../../../images/skills-icons/mysql.png";
 import BoostrapIcon from "../../../../images/skills-icons/boostrap.png";
 import NextJSIcon from "../../../../images/skills-icons/nextjs.png";
+import PostgreSQLIcon from "../../../../images/skills-icons/Postgresql.png";
+import PHPIcon from "../../../../images/skills-icons/php.png";
+import GraphQL from "../../../../images/skills-icons/graphql.png";
 import Skill from "./Skill";
 
-const toLearnSkillsIcons = [{
-  id: 1,
-  icon: BoostrapIcon,
-  name: "Boostrap"
-},
-{
-  id: 2,
-  icon: NextJSIcon,
-  name: "Nextjs"
-},
-{
-  id: 3,
-  icon: MySQLIcon,
-  name: "MySQL"
-}]
+const toLearnSkillsIcons = [
+  {
+    id: 1,
+    icon: BoostrapIcon,
+    name: "Boostrap",
+  },
+  {
+    id: 2,
+    icon: NextJSIcon,
+    name: "Nextjs",
+  },
+  {
+    id: 3,
+    icon: MySQLIcon,
+    name: "MySQL",
+  },
+  {
+    id: 4,
+    icon: PostgreSQLIcon,
+    name: "PostgreSQL",
+  },
+  {
+    id: 5,
+    icon: PHPIcon,
+    name: "PHP",
+  },
+  {
+    id: 6,
+    icon: GraphQL,
+    name: "GraphQL",
+  },
+];
 
 const SkillsToLearn = () => {
   return (
     <>
-      {toLearnSkillsIcons.map(skill => (
+      {toLearnSkillsIcons.map((skill) => (
         <Skill id={skill.id} icon={skill.icon} name={skill.name} />
       ))}
     </>
   );
-}
-
-
-
-
-
-
-
-
+};
 
 export default SkillsToLearn;
